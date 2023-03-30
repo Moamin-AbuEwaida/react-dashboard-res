34015a0650a465ae6be2abef9b87fe9a1bb103e6
Moamin <moamin.salamah@gmail.com>
2023-03-30 13:50:57 +0200

    calendar page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Layout from './components/layout/Layout';
+import Calendar from './pages/calendar/Calendar';
 import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route path='dashboard' element={<Dashboard />} />
+            <Route path='calendar' element={<Calendar /> } />
           </Route>
+          
         </Routes>
       </BrowserRouter>
     </div>
