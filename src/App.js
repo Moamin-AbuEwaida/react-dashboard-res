718467fce1946d50f56bd826c70f4d3538794796
Moamin <moamin.salamah@gmail.com>
2023-03-30 17:12:01 +0200

    not completed board page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Layout from './components/layout/Layout';
+import BoardPage from './pages/board/Board';
 import Calendar from './pages/calendar/Calendar';
 import Dashboard from './pages/dashboard/Dashboard';
 
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Layout />}>
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='calendar' element={<Calendar /> } />
+            <Route path='board' element={<BoardPage /> } />
           </Route>
           
         </Routes>
